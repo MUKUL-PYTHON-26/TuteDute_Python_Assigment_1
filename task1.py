e3fd5a77975d2a704c8980f121b9ad7db2223417
MUKUL-PYTHON-26 <lifestudent2025@gmail.com>
2026-02-02 22:41:57 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,10 @@
+# Task 1: Basic Mathematical Operations
+# User se do numbers input lena
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Operations perform karna aur result dikhana
+print(f"Addition: {num1 + num2}")
+print(f"Subtraction: {num1 - num2}")
+print(f"Multiplication: {num1 * num2}")
+print(f"Division: {num1 / num2}")
---
+++ task2.py
@@ -0,0 +1,7 @@
+# Task 2: Create a Personalized Greeting
+# User se uska Pehla aur Aakhri naam input lena
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+# Welcome message print karna (f-string ka use karke)
+print(f"Hello, {first_name} {last_name}! Welcome to the Python course.")
